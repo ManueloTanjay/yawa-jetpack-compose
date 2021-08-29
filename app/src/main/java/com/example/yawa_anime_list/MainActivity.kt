a0b301f7ac1574d9345ee4892dca4b96929e04d1
Manuelo <57776467+ManueloTanjay@users.noreply.github.com>
2021-08-29 14:07:45 -0400

    installd and configures Apollo3; queries still need to be tested; ran into filenotfound error that was fixed by cleaning and rebuilding project

--- app/src/main/java/com/example/yawa_anime_list/MainActivity.kt
+++ app/src/main/java/com/example/yawa_anime_list/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.yawa_anime_list
 
+import GetUserMediaListOptionsQuery
+import GetViewerQuery
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
@@ -18,6 +20,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.http.HttpRequest
+import com.apollographql.apollo3.api.http.HttpResponse
+import com.apollographql.apollo3.api.http.withHeader
+import com.apollographql.apollo3.exception.ApolloException
+import com.apollographql.apollo3.network.http.HttpInterceptor
+import com.apollographql.apollo3.network.http.HttpInterceptorChain
+import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.example.yawa_anime_list.ui.theme.User
 import com.example.yawa_anime_list.ui.theme.YawaanimelistTheme
 import kotlinx.coroutines.launch
 
@@ -29,7 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         //get url with session token and bearer from intent
-        val action: String? = intent?.action;
+        val action: String? = intent?.action
         val data: Uri? = intent?.data
         //get session token from url
         var sessionToken: String? = data?.fragment?.split("=")?.get(1)?.split("&")?.get(0)
@@ -39,7 +50,7 @@ class MainActivity : ComponentActivity() {
         val sharedPreferences = getSharedPreferences("yawa", MODE_PRIVATE)
         val editor = sharedPreferences.edit()
 
-        lifecycleScope.launch() {
+        lifecycleScope.launch {
             if (hasLoggedIn(sharedPreferences)) {
                 sessionToken = sharedPreferences.getString("sessionToken", null)
                 sTokenExpiration = sharedPreferences.getString("sessionTokenExpiry", null)
@@ -70,6 +81,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 
 /**
  *  check if session token for user already exists
@@ -118,5 +131,51 @@ fun ListsScreen(sessionToken: String, sTokenExpiration: String) {
 fun DefaultPreview() {
     YawaanimelistTheme {
         LoginScreen()
+    }
+}
+
+suspend fun getUserInfo(session_token: String): User? {
+    val TAG = "GetUserInfoTAG"
+    var userInfo = User()
+    val apolloClient = ApolloClient(
+        networkTransport = HttpNetworkTransport(
+            serverUrl = "https://graphql.anilist.co/",
+            interceptors = listOf(AuthorizationInterceptor(session_token))
+        )
+    )
+    //get username and userID
+    val viewerQueryResponse = try {
+        apolloClient.query(GetViewerQuery())
+    } catch (e: ApolloException) {
+        Log.d("GETUSERINFO", e.toString())
+        return null
+    }
+
+    val viewer = viewerQueryResponse.data?.viewer
+    if (viewer == null || viewerQueryResponse.hasErrors())
+        return null
+
+    userInfo.username = viewer.name
+    userInfo.userID = viewer.id.toString()
+    Log.d(TAG, "${viewer.id} ${viewer.name}")
+    //get userMediaListOptions
+    val userMediaListOptionsResponse = try {
+        apolloClient.query(GetUserMediaListOptionsQuery(userInfo.userID.toInt()))
+    } catch (e: ApolloException) {
+        Log.d("GETUSERMEDISLISTOPTIONS", e.toString())
+        return null
+    }
+
+    val user = userMediaListOptionsResponse.data?.user
+    if (user == null || userMediaListOptionsResponse.hasErrors())
+        return null
+
+    userInfo.userMediaListOptions = user.mediaListOptions?.scoreFormat?.rawValue.toString()
+    return userInfo
+}
+
+class AuthorizationInterceptor(val token: String) : HttpInterceptor {
+    override suspend fun intercept(request: HttpRequest, chain: HttpInterceptorChain): HttpResponse {
+        return chain.proceed(request.withHeader("Authorization", "Bearer $token"))
     }
 }
---
+++ app/src/main/java/com/example/yawa_anime_list/ui/theme/User.kt
@@ -0,0 +1,7 @@
+package com.example.yawa_anime_list.ui.theme
+
+class User {
+    var username = ""
+    var userID = ""
+    var userMediaListOptions = ""
+}
