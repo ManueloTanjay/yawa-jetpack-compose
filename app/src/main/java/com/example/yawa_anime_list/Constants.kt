7a3e6600ef1321eb41c3bdca8c489b9d79938059
Manuelo <57776467+ManueloTanjay@users.noreply.github.com>
2021-08-31 20:50:46 -0400

    Constants.kt created to store project constants. Added MediaListStatus fields to Constants.

---
+++ app/src/main/java/com/example/yawa_anime_list/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.yawa_anime_list
+
+class Constants {
+
+    //  MediaListStatus for mediaList queries, paired with username or userID, query authenticated with sessionToken
+    val CURRENT = "CURRENT"
+    val PLANNING = "PLANNING"
+    val COMPLETED = "COMPLETED"
+    val DROPPED = "DROPPED"
+    val PAUSED = "PAUSED"
+    val REPEATING = "REPEATING"
+
+}
